07f7c0f48ec78c582ea99e3c5b62955f89e33809
damonweng <damonweng@outlook.com>
2015-02-03 20:01:44 -0500

    init

---
+++ sampleCode/src/main/java/task8/TestServlet.java
@@ -0,0 +1,31 @@
+package task8;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class TestServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		PrintWriter writer = resp.getWriter();
+		writer.println("WWWW");
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		doGet(req, resp);
+	}
+
+}
